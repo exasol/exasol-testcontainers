22c8303cd490c785c995d4d4304f1fbe14f19f45
Sebastian Bär <sebastian.baer@exasol.com>
2020-07-10 13:56:53 +0200

    #68: Fixed JavaDoc.

--- src/main/java/com/exasol/containers/ExasolDockerImageReference.java
+++ src/main/java/com/exasol/containers/ExasolDockerImageReference.java
@@ -32,13 +32,13 @@ public class ExasolDockerImageReference {
      * Create a new instance of an {@link ExasolDockerImageReference} by parsing a reference string.
      * <p>
      * The following shortened reference strings are are supported and reference the standard Exasol {@code docker-db}:
+     * </p>
      * <ul>
      * <li>&lt;major&gt; (7)</li>
      * <li>&lt;major&gt;.&lt;minor&gt; (7.1)</li>
      * <li>&lt;major&gt;.&lt;minor&gt;.&lt;fix&gt; (7.1.5)</li>
      * <li>&lt;major&gt;.&lt;minor&gt;.&lt;fix&gt;-&lt;docker-image-revision&gt; (7.1.5-d2)</li>
      * </ul>
-     * </p>
      * <p>
      * Anything else is treated like a regular Docker image reference string.
      * </p>
