cf086e4313109a5bc58e6a27af5a11ef5fea43d8
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-09-09 10:32:34 +0200

    #161: Add log messages for support package creation

--- src/main/java/com/exasol/containers/ExasolContainer.java
+++ src/main/java/com/exasol/containers/ExasolContainer.java
@@ -707,6 +707,9 @@ public class ExasolContainer<T extends ExasolContainer<T>> extends JdbcDatabaseC
     private void collectSupportInformation(final ExitType exitType) {
         if (this.dockerImageReference.hasMajor() && (this.dockerImageReference.getMajor() >= 7)) {
             this.supportInformationRetriever.run(exitType);
+        } else {
+            LOGGER.info("Skipping support information retrieval for version {}, only supported with version >= 7",
+                    this.dockerImageReference);
         }
     }
 
