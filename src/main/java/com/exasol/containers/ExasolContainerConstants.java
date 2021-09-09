c387a6e6e9b5a5af96298a8317297c1acb3882c5
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-09-09 13:26:40 +0200

    #161: Update default exasol docker image version

--- src/main/java/com/exasol/containers/ExasolContainerConstants.java
+++ src/main/java/com/exasol/containers/ExasolContainerConstants.java
@@ -6,7 +6,7 @@ import java.util.Set;
  * Constants for the Exasol Docker containers (like image ID and version).
  */
 public final class ExasolContainerConstants {
-    public static final String EXASOL_DOCKER_IMAGE_VERSION = "7.0.10";
+    public static final String EXASOL_DOCKER_IMAGE_VERSION = "7.1.0-d1";
     public static final String EXASOL_DOCKER_IMAGE_ID = "exasol/docker-db";
     public static final String EXASOL_DOCKER_IMAGE_REFERENCE = EXASOL_DOCKER_IMAGE_ID + ":"
             + EXASOL_DOCKER_IMAGE_VERSION;
