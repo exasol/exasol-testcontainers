e06da45d96db29fc0ba6c8d4e35ca8bfea61adae
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-09-09 16:02:44 +0200

    Revert "#161: Rename support archive to include test name"
    This reverts commit 9bdaa83dfdab4e8233b3da7cf5b2ed4e56ef7313.

--- src/main/java/com/exasol/support/SupportInformationRetriever.java
+++ src/main/java/com/exasol/support/SupportInformationRetriever.java
@@ -3,7 +3,6 @@ package com.exasol.support;
 import static org.testcontainers.containers.BindMode.READ_WRITE;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.slf4j.Logger;
@@ -94,14 +93,8 @@ public class SupportInformationRetriever {
         try {
             final ExecResult result = this.container.execInContainer(EXASUPPORT_EXECUTABLE);
             if (result.getExitCode() == ExitCode.OK) {
-                final String filename = extractFilenameFromConsoleMessage(result);
-                Path hostPath = getHostPath(filename);
-                if (testDescription != null) {
-                    final Path pathWithDescription = hostPath.getParent()
-                            .resolve(testDescription + "_" + hostPath.getFileName().toString());
-                    Files.move(hostPath, pathWithDescription);
-                    hostPath = pathWithDescription;
-                }
+                final String filename = getFilename(result, testDescription);
+                final String hostPath = getHostPath(filename);
                 logSuccessfulArchiveCreationAttempt(exitType, hostPath);
             } else {
                 logFailedSupportArchiveCreationAttempt(exitType, result.getStderr());
@@ -114,8 +107,16 @@ public class SupportInformationRetriever {
         }
     }
 
+    private String getFilename(final ExecResult result, final String testDescription) {
+        if (testDescription == null) {
+            return extractFilenameFromConsoleMessage(result);
+        } else {
+            return testDescription + "_" + extractFilenameFromConsoleMessage(result);
+        }
+    }
+
     @SuppressWarnings("java:S2629")
-    private void logSuccessfulArchiveCreationAttempt(final ExitType exitType, final Path pathOfArchiveOnHost) {
+    private void logSuccessfulArchiveCreationAttempt(final ExitType exitType, final String pathOfArchiveOnHost) {
         LOGGER.info("Container exiting with {}. Monitoring is set to {}. Wrote support archive to: {}", exitType,
                 this.monitoredExitType, pathOfArchiveOnHost);
     }
@@ -125,8 +126,8 @@ public class SupportInformationRetriever {
         return consoleMessage.substring(consoleMessage.indexOf(SUPPORT_ARCHIVE_PREFIX));
     }
 
-    private Path getHostPath(final String filename) {
-        return this.targetDirectory.resolve(filename);
+    private String getHostPath(final String filename) {
+        return this.targetDirectory.resolve(filename).toString();
     }
 
     @SuppressWarnings("java:S2629")
