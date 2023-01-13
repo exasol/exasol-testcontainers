31acc5078bbb8663ad508ab7f53a533ffd7b1b14
KK <christoph.kuhnke@exasol.com>
2023-01-13 15:20:56 +0100

    javadoc

--- src/main/java/com/exasol/containers/ssh/DirectorySelector.java
+++ src/main/java/com/exasol/containers/ssh/DirectorySelector.java
@@ -11,30 +11,51 @@ import java.nio.file.Path;
 public class DirectorySelector {
 
     private final Path parent;
-
     private boolean found = false;
     private Path path;
     private boolean knownAsExisting = false;
 
     private static final Path CURRENT_WORKING_DIRECTORY = Path.of("");
 
+    /**
+     * Create a new instance of {@link DirectorySelector}.
+     */
     public DirectorySelector() {
         this(CURRENT_WORKING_DIRECTORY);
     }
 
-    public DirectorySelector(final Path parent) {
+    DirectorySelector(final Path parent) {
         this.parent = parent;
     }
 
+    /**
+     * Add a candidate path if path is not null.
+     *
+     * @param path path to add
+     * @return this for fluent programming
+     */
     public DirectorySelector ifNotNull(final Path path) {
         return consider(path != null, path, false);
     }
 
+    /**
+     * Add a path relative to current working directory as candidate if the path exists.
+     *
+     * @param name name of the directory relative to current working directory
+     * @return this for fluent programming
+     */
     public DirectorySelector orIfExists(final String name) {
         final Path path = this.parent.resolve(name);
         return consider(Files.isDirectory(path), path, true);
     }
 
+    /**
+     * Add a path relative to current working directory as candidate unconditionally. Method {@link #ensureExists()}
+     * will create this path if it does not exist.
+     *
+     * @param name name of the directory relative to current working directory
+     * @return this for fluent programming
+     */
     public DirectorySelector or(final String name) {
         return consider(true, this.parent.resolve(name), false);
     }
@@ -52,6 +73,13 @@ public class DirectorySelector {
         return this.path;
     }
 
+    /**
+     * Ensure that the current candidate for directory selection exists. If it not known to have been existing before
+     * and does not exist yet then create the directory including all parent directories and return the path.
+     *
+     * @return instance of {@link Path} pointing to the existing directory
+     * @throws UncheckedIOException in case the creation of the path failed
+     */
     public Path ensureExists() throws UncheckedIOException {
         if (this.knownAsExisting || Files.isDirectory(this.path)) {
             return this.path;
