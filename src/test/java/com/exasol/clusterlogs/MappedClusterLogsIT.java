338dba9ff0e2496174cc2b1ec5afc20f6ce53bd9
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-11-07 15:16:12 +0000

    Simplify test

--- src/test/java/com/exasol/clusterlogs/MappedClusterLogsIT.java
+++ src/test/java/com/exasol/clusterlogs/MappedClusterLogsIT.java
@@ -3,12 +3,13 @@ package com.exasol.clusterlogs;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.io.*;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -17,31 +18,19 @@ import com.exasol.containers.ExasolContainer;
 @Tag("slow")
 @Testcontainers
 class MappedClusterLogsIT {
-    private static final Path TEMP_DIR = createTempDir();
 
-    private static Path createTempDir() {
-        final Path tempDir = createTempPath();
-        tempDir.toFile().deleteOnExit();
-        return tempDir;
-    }
-
-    private static Path createTempPath() {
-        try {
-            return Files.createTempDirectory(MappedClusterLogsIT.class.getName());
-        } catch (final IOException exception) {
-            throw new UncheckedIOException(exception);
-        }
-    }
+    @TempDir
+    private static Path tempDir;
 
     @Container
-    private static final ExasolContainer<? extends ExasolContainer<?>> CONTAINER = new ExasolContainer<>() //
-            .withClusterLogsPath(TEMP_DIR) //
+    private static final ExasolContainer<? extends ExasolContainer<?>> CONTAINER = new ExasolContainer<>("8.23.0") //
+            .withClusterLogsPath(tempDir) //
             .withRequiredServices();
 
     @Test
     // [itest->dsn~mapping-the-log-directory-to-the-host~1]
     void testMapClusterLogs() throws InterruptedException, IOException {
-        final File syslogFile = TEMP_DIR.resolve("syslog").toFile();
+        final File syslogFile = tempDir.resolve("syslog").toFile();
         assertThat("File " + syslogFile + " exists", syslogFile.exists(), equalTo(true));
     }
 }
